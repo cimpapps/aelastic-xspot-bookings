ce8051b6dc706fc4e6ce17da9afc512266d894e7
cimpapps <catalincimpoeru24@gmail.com>
2019-01-22 23:24:42 +0200

    add requests validation

--- src/main/java/com/aelastic/xspot/bookings/models/Booking.java
+++ src/main/java/com/aelastic/xspot/bookings/models/Booking.java
@@ -6,7 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Set;
@@ -17,16 +21,26 @@ import java.util.Set;
 @Builder
 public class Booking implements Serializable {
 
+    @NotNull
+    @NotEmpty
     private String bookingId;
 
+    @NotNull
+    @NotEmpty
     private String userID;
 
+    @NotNull
+    @NotEmpty
     private String placeID;
 
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDateTime startDate;
 
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDateTime endDate;
 
+    @NotNull
+    @Positive
     private int nrOfPeople;
 
     private Set<String> friends;
