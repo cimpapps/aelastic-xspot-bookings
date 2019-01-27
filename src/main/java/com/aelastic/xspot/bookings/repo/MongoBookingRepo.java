053b595b27fce396c70bb3ec9ece987432938435
cimpapps <catalincimpoeru24@gmail.com>
2019-01-27 22:31:47 +0200

    fix query dsl predicate --- excluded

--- src/main/java/com/aelastic/xspot/bookings/repo/MongoBookingRepo.java
+++ src/main/java/com/aelastic/xspot/bookings/repo/MongoBookingRepo.java
@@ -2,11 +2,9 @@ package com.aelastic.xspot.bookings.repo;
 
 import com.aelastic.xspot.bookings.models.dao.Booking;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 
-public interface MongoBookingRepo extends MongoRepository<Booking, String>, QuerydslPredicateExecutor {
-
+public interface MongoBookingRepo extends MongoRepository<Booking, String> {
 
 
 }
