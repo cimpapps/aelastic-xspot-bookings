32bf79d79069550baa13fad80705ef9aafb8fa74
cimpapps <catalincimpoeru24@gmail.com>
2019-02-02 00:51:15 +0200

    tests and refactory

--- src/test/java/com/aelastic/xspot/bookings/service/impl/BookingServiceImplTests.java
+++ src/test/java/com/aelastic/xspot/bookings/service/impl/BookingServiceImplTests.java
@@ -1,8 +1,10 @@
 package com.aelastic.xspot.bookings.service.impl;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore
 @RunWith(SpringRunner.class)
 public class BookingServiceImplTests {
 }
