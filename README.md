70eb06ac5e0f8cf5821ca5c8984723dd6c06817e
cimpapps <catalincimpoeru24@gmail.com>
2019-03-01 17:58:03 +0200

    click dreapta dilit

--- README.md
+++
@@ -1,1 +0,0 @@
-# aelastic-xspot-bookings
--- src/main/java/com/aelastic/xspot/bookings/models/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.aelastic.xspot.bookings.models;
-
-public class Constants {
-
-    public static final String DATE_FORMAT = "dd-MM-yyyy hh:mm:00";
-
-}
